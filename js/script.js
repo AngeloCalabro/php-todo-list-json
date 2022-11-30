6431f7d1f36da19c48642abb61216ba8f628ef70
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-30 18:38:37 +0100

    funzione add non va

--- js/script.js
+++ js/script.js
@@ -3,8 +3,38 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            message: 'Hello Vue!',
-
+            title: 'Todo List',
+            newTodoText: '',
+            todoList: [],
+            apiUrl: './server.php'
         }
-    }
+    },
+    methods: {
+        getTodo() {
+            axios.get(this.apiUrl).then(
+                (response) => {
+                    // console.log(response.data);
+                    this.todoList = response.data;
+                })
+        },
+        addTodo(){
+            const data = {
+                newTodoText: this.newTodoText,
+            }
+            axios.post(
+                this.apiUrl, 
+                data,
+                {'Content-Type': 'multipart/form-data'}
+            ).then(
+                (response) => {
+                    // console.log(response.data);
+                    
+                    this.newTodoText = '';
+                    this.getTodo();
+                })
+        }
+    },
+    mounted() {
+        this.getTodo();
+    },
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,7 +1,34 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-Requested-With");
+$file_url = './data.json';
 
-$toDo = file_get_contents('data.json');
+$file_text = file_get_contents($file_url);
+$todo_list = json_decode($file_text);
 
-header('Content-Type: application/json');
+if(isset($_POST['newTodoText'])){
+    // echo "il parametro post è arrivato";
+    //prendere il valore inviatoci
+    //inserirlo nell'array
+    //salvarlo nel file
+    //ricaricare i nuovi todo
 
-echo json_encode($toDo);
+    // $newTodoText = $_POST['newTotoText'];
+    $newTodo = [
+        'text' => $_POST['newTodoText'],
+        'done' => false
+    ];
+
+    array_push($todo_list, $newTodo);
+    // print_r($todo_list);
+
+    file_put_contents($file_url, json_encode($todo_list));
+
+    // header('Content-Type: application/json');
+    // echo json_encode($todo_list);
+} else {
+    // echo 'il parametro non è arrivato';
+    header('Content-Type: application/json');
+    echo json_encode($todo_list);
+};
+
